2ffc81a9f442f02fe191f0db7a6fd5f810c539aa
Mark Dyer <papamarky@markyshouse.com>
2016-01-30 12:47:52 -0800

    DigitalClock: Add basics of brightness control. Add logging.

--- src/DigitalClock.py
+++ src/DigitalClock.py
@@ -6,8 +6,33 @@ import time
 import digit_defs as d
 import RPi.GPIO as GPIO
 import BigDisplay as B
+import logging
+
+# Set up main logging stuff
+logger = logging.getLogger('BigClock')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('/var/log/BigClock.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
+
 
 display = B.BigDisplay(16, 21, 20)
+
+# brightness circuit test
+import select
+pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
+dc = 50     # duty cycle (0 - 100)
+
+GPIO.setup(pwmPin, GPIO.OUT)
+pwm = GPIO.PWM(pwmPin, 50)
 
 _hour = -1
 _min = -1
@@ -60,9 +85,38 @@ def displayTime(now):
 def cleanUp():
     display.clear_all()
     GPIO.cleanup()
-    
+
+# brightness circuit test
+pwm.start(dc)
+
+logger.info("Starting")
+logger.info("Brightness: %d%%", dc)
+
 try:
     while True:
+        while False and sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            line = sys.stdin.readline().rstrip()
+            print "LINE: '{}'".format(line)
+            new_dc = dc
+            if line.isdigit():
+                new_dc = int(line)
+            else:
+                for c in line:
+                    if c is '=' or c is '+':
+                        new_dc = new_dc + 1
+                    elif c is '-' or c is '_':
+                        new_dc = new_dc - 1
+
+            if new_dc > 100:
+                new_dc = 100
+            elif new_dc < 0:
+                new_dc = 0
+
+            if new_dc != dc:
+                dc = new_dc
+                print "DC: {}".format(dc)
+                pwm.ChangeDutyCycle(dc)
+
         now = dt.now()
         if _hour != now.hour or _min != now.minute or _sec != now.second:
             _hour = now.hour
@@ -73,6 +127,7 @@ try:
 
         time.sleep(0.1)
 except KeyboardInterrupt:
+    logger.info("KeyboardInterrupt")
     cleanUp()
     sys.exit(0)
 
@@ -80,7 +135,10 @@ except Exception as inst:
     print "Exception: " + str(type(inst))
     print inst.args
     print inst
+    logger.error("Exception: {}", str(type(inst)))
+    logger.error(inst.args)
+    logger.error(inst)
     cleanUp()
 
-print "All Done."
-cleanup()
+logger.info("All Done")
+cleanUp()
